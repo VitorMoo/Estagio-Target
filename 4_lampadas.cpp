c1d2e030c277686503711afd6a3e1d04862fef47
VitorMoo <vitorrmoreria@outlook.com>
2024-03-06 21:15:43 -0300

    nao tinha adicionado a resposta do 4

--- 4_lampadas.cpp
+++ 4_lampadas.cpp
@@ -4,5 +4,13 @@
 
 Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas,
  qual interruptor controla cada lâmpada?
+
+ RESPOSTA:
+ Existem 2 interruptores, A e B. Se deixarmos o interruptor A ligado por um tempo para gerar calor e depois desligarmos, em seguida ligamos o interruptor B. Se a primeira sala estiver com a lâmpada acesa,
+significa que o interruptor B controla essa lâmpada, logo B=1.
+
+Se a lâmpada não estiver ligada, vamos verificar a temperatura da lâmpada.
+Se a lâmpada estiver quente, significa que o interruptor A estava ligado, logo A=1.
+ 
+Se ela estiver fria, o interruptor A estava desligado, logo C=1.
 */
- // 0..1  0+1=1  1..1  1+1=2
